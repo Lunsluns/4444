5d643a48c4bfcc7189d788b7c2410fb03953c6dd
Unknown <bsylve1996@gmail.com>
2018-10-20 17:07:57 -0500

    removed the commented out code
    yes

--- run.py
+++ run.py
@@ -1,22 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-from __future__ import absolute_import
-from __future__ import division, print_function, unicode_literals
-
-from parsers.plaintext import PlaintextParser #We're choosing a plaintext parser here, other parsers available for HTML etc.
-from nlp.tokenizers import Tokenizer
-from summarizers.lex_rank import LexRankSummarizer #We're choosing Lexrank, other algorithms are also built in
-
-file = "plain_text.txt" #name of the plain-text file
-parser = PlaintextParser.from_file(file, Tokenizer("english"))
-summarizer = LexRankSummarizer()
-
-summary = summarizer(parser.document, 5) #Summarize the document with 5 sentences
-
-for sentence in summary:
-    print (sentence)
-"""
-# -*- coding: utf-8 -*-
 
 from __future__ import absolute_import
 from __future__ import division, print_function, unicode_literals
