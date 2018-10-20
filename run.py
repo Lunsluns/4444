8c226a3c57b217ebbe90f1644284b6f815ec497c
meganopolis <mowen22@lsu.edu>
2018-10-20 15:30:05 -0500

    Add files via upload

---
+++ run.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division, print_function, unicode_literals
+
+from sumy.parsers.plaintext import PlaintextParser #We're choosing a plaintext parser here, other parsers available for HTML etc.
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lex_rank import LexRankSummarizer #We're choosing Lexrank, other algorithms are also built in
+
+file = "plain_text.txt" #name of the plain-text file
+parser = PlaintextParser.from_file(file, Tokenizer("english"))
+summarizer = LexRankSummarizer()
+
+summary = summarizer(parser.document, 5) #Summarize the document with 5 sentences
+
+for sentence in summary:
+    print (sentence)
