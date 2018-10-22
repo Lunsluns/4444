d8b648e5c1d3a197e25399855a05a8de1d6f5a89
Garen Crownguard <22408776+Garen7@users.noreply.github.com>
2018-10-21 20:27:30 -0500

    Basic UI
    Basic UI implimentations using tkinter.

--- run.py
+++ run.py
@@ -12,6 +12,8 @@ from summarizers.text_rank import TextRankSummarizer as textSum
 from nlp.stemmers import Stemmer
 from utils import get_stop_words
 
+from tkinter import *
+
 
 LANGUAGE = "english"
 SENTENCES_COUNT = 10
@@ -20,31 +22,34 @@ SENTENCES_COUNT = 10
 if __name__ == "__main__":
     #url = "http://www.zsstritezuct.estranky.cz/clanky/predmety/cteni/jak-naucit-dite-spravne-cist.html"
     url = "https://www.npr.org/2018/10/21/658921379/futuristic-dreams-turn-to-nightmare-in-electric-state"
-    parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
-    # or for plain text files
-    # parser = PlaintextParser.from_file("document.txt", Tokenizer(LANGUAGE))
-    stemmer = Stemmer(LANGUAGE)
 
-    summarizer = lexSum(stemmer)
-    summarizer.stop_words = get_stop_words(LANGUAGE)
+    root = Tk()
+    e = Entry(root)
+    e.pack()
 
-    for sentence in summarizer(parser.document, SENTENCES_COUNT):
-        print(sentence)
+    e.focus_set()
 
-    print('')
-    print('')
-    
-    summarizer = luhnSum(stemmer)
-    summarizer.stop_words = get_stop_words(LANGUAGE)
+    def display(label, parser, summarizer):
+        label.delete(1.0, END)
+        summarizer.stop_words = get_stop_words(LANGUAGE)
 
-    for sentence in summarizer(parser.document, SENTENCES_COUNT):
-        print(sentence)
+        for sentence in summarizer(parser.document, SENTENCES_COUNT):
+            label.insert(END, sentence)
 
-    print('')
-    print('')
+    def callback():
+        url = e.get()
+        newWindow = Tk()
+        mainDisplay = Text(newWindow)
+        mainDisplay.pack()
+        buttonLex = Button(newWindow, text="Lex", command=lambda: display(mainDisplay, HtmlParser.from_url(url, Tokenizer(LANGUAGE)), lexSum(Stemmer(LANGUAGE))))
+        buttonLex.pack()
+        buttonLuhn = Button(newWindow, text="Luhn", command=lambda: display(mainDisplay, HtmlParser.from_url(url, Tokenizer(LANGUAGE)), luhnSum(Stemmer(LANGUAGE))))
+        buttonLuhn.pack()
+        buttonText = Button(newWindow, text="Text", command=lambda: display(mainDisplay, HtmlParser.from_url(url, Tokenizer(LANGUAGE)), textSum(Stemmer(LANGUAGE))))
+        buttonText.pack()
+        newWindow.mainloop()
 
-    summarizer = textSum(stemmer)
-    summarizer.stop_words = get_stop_words(LANGUAGE)
+    b = Button(root, text="Summarize", width=40, command=callback)
+    b.pack()
 
-    for sentence in summarizer(parser.document, SENTENCES_COUNT):
-        print(sentence)
+    mainloop()
