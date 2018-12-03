f848797d94081a1b213d69eb8b793bcbe15411a9
Unknown <bsylve1996@gmail.com>
2018-12-02 18:32:24 -0600

    Fixed temprun.py

--- temprun.py
+++ temprun.py
@@ -7,7 +7,7 @@ from nlp.tokenizers import Tokenizer
 from summarizers.lex_rank import LexRankSummarizer as lexSum
 from summarizers.luhn import LuhnSummarizer as luhnSum
 from summarizers.text_rank import TextRankSummarizer as textSumi
-from summarizers.AI_Algorithm import ourSummarizer as jackieSum
+from summarizers.AI_Algorithm import ourSummarizer as ourSum
 from nlp.stemmers import Stemmer
 from utils import get_stop_words
 
@@ -21,6 +21,7 @@ SENTENCES_COUNT = 10
 if __name__ == "__main__":
 
     url = "https://www.livescience.com/64184-mini-placentas-lab-grown.html"
+    parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
     # or for plain text files
     # parser = PlaintextParser.from_file("document.txt", Tokenizer(LANGUAGE))
     stemmer = Stemmer(LANGUAGE)
